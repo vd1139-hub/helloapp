3b2c13034134d66abb82a8a7b5ac5a903f209e0e
Maheshkumar <vd1139@srmist.edu.in>
2026-03-18 08:48:45 +0530

    feature/UC3-display-name-default

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
 public class HelloApp {
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
-        String name = sc.nextLine();
+        String name = sc.nextLine();   // taking input
 
         System.out.println("Hello, " + name + "!");
-
-        sc.close();
     }
 }
